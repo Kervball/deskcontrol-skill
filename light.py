93400602536e2ad083b006668b2f0fe68c0c27f9
James <jameskervin.2012@gmail.com>
2020-07-21 16:40:08 -0400

    atempt 19

--- light.py
+++ light.py
@@ -1,12 +1,12 @@
 import RPi.GPIO as GPIO
 #useless edit so I can push again
 
-red_front = 13
-green_front = 6
+red_front = 16
+green_front = 20
 blue_front = 27
 
 red_back = 22
-green_back = 5
+green_back = 21
 blue_back = 17
 
 GPIO.setwarnings(False)
@@ -70,7 +70,7 @@ def off(location):
     GPIO.output(blue_off,GPIO.LOW)
 #----------set color fucntion
 def set(location, color):
-    if location == "Front":
+    if location == "FRONT LIGHTS":
         if color == "red":
             red(red_front, blue_front, green_front)
         elif color == "blue":
@@ -85,7 +85,7 @@ def set(location, color):
             yellow(red_front, blue_front, green_front)
         elif color == "teal":
             teal(red_front, blue_front, green_front)
-    elif location == "Back":
+    elif location == "BACK LIGHTS":
         if color == "red":
             red(red_back, blue_back, green_back)
         elif color == "blue":
@@ -100,7 +100,7 @@ def set(location, color):
             yellow(red_back, blue_back, green_back)
         elif color == "teal":
             teal(red_back, blue_back, green_back)
-    elif location == "Both":
+    elif location == "BOTH LIGHTS":
         if color == "red":
             red(red_front, blue_front, green_front)
             red(red_back, blue_back, green_back)
